20674441a01eb65f15214195c9dd8cd2d8aa6d9f
Shady Aboulela <contact@apexsmeai.com>
2026-04-01 16:20:03 +0300

    Update web build with Sprint 5 + Sprint 6 updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "528040d908c448ef4b63d818b2391283
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "1c1366c6f8def735c8fd2d8807462d2e",
+"flutter_bootstrap.js": "70dff860a21b4a5d0570cd0c373c8026",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
