e88003136f12607db0b80d16c93113a427374482
Shady Aboulela <contact@apexsmeai.com>
2026-04-05 21:55:10 +0300

    deploy: v6.5 21:55

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3901215071"
+    serviceWorkerVersion: "3519065177"
   }
 });
