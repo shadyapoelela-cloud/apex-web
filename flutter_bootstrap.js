c2f76a8778636f829b844b117039dc049f1be3e5
Shady Aboulela <contact@apexsmeai.com>
2026-04-05 07:54:51 +0300

    deploy: Dashboard with charts 07:54

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "66271504"
+    serviceWorkerVersion: "3912331855"
   }
 });
